e281bdadc0548173b212bfaeeb7e22f8fc64030c
mohamednabilmahdy <mohamed.n.mahdy2@gmail.com>
2022-07-18 07:12:50 +0200

    first commit

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
 const port = process.env.port || 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
   })
 
 //   app.listen(port, () => {
